af555138fb292d8d7f9e85822c743c59b1ba842b
ry-rise <ry-rise@example.com>
2018-10-18 21:25:25 +0900

    仮組み

---
+++ Assets/Script/Click.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Click : MonoBehaviour {
+    private int score;
+    private Text scoretext;
+
+    void Start()
+    {
+        scoretext = GameObject.Find("Text_Score").GetComponent<Text>();
+        scoretext.text = "0 G";
+    }
+
+    void Update ()
+    {
+        // TODO: 毎フレーム計算するの無駄
+        // score が変更されたときに↓の計算する
+        //scoretext.text = score.ToString() + " Byte";
+	}
+
+    public void OnClick()
+    {
+        if (transform.name == "Button_Increase")
+        {
+            score += 1;
+            scoretext.text = score.ToString() + " G";
+        }
+        if (transform.name == "Button_Store")
+        {
+            SceneManager.LoadScene("");
+        }
+        if (transform.name == "Button_Desktop")
+        {
+            SceneManager.LoadScene("");
+        }
+    }
+}
---
+++ Assets/Script/Store.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Store : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
